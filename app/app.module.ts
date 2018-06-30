e3e8ad892ceecd2d31d9024565f15dc93ef9d871
Fanchao92 <fanchaozhou1992@gmail.com>
2018-06-30 13:57:20 -0700

    Imported MediaItemComponent

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    MediaItemComponent
+  ],  // NOTE: Components declared in the root module are available to all components in this app
   bootstrap: [
     AppComponent
   ]
