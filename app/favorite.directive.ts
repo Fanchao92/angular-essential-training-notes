cab9fdd9dafed7f961bd74d3e7f19aae3f8c4cd6
Fanchao92 <fanchaozhou1992@gmail.com>
2018-07-01 14:14:49 -0700

    custom directive: changed some notes

--- app/favorite.directive.ts
+++ app/favorite.directive.ts
@@ -6,6 +6,11 @@ import { Directive, HostBinding } from '@angular/core'
 export class FavoriteDirective {
 	// NOTE: The directive will be bound to the HTML element
 	// NOTE: HostBinding can link an internal property to an input property on the host HTML element
-	// NOTE: The binding will occur when the variable evaluates to true
+	/* NOTE: 
+	   The value of the property bound to the host HTML element is determined by the internal property.
+	   For example:
+	   @HostBinding('style.color') color: string;
+	   Then, the color of the host HTML element will be defined by the 'color' variable here.
+	*/ 
 	@HostBinding('class.is-favorite') isFavorite = true;
 }
