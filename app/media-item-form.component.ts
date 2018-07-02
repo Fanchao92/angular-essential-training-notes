fbb50f8ed36697d43f95d272f3154bc5cdd56ffc
Fanchao92 <fanchaozhou1992@gmail.com>
2018-07-01 22:22:11 -0700

    model-driver forms

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'mw-media-item-form',
@@ -6,7 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['app/media-item-form.component.css']
 })
 export class MediaItemFormComponent {
+  form: FormGroup;
+
+  ngOnInit() {
+  	this.form = new FormGroup({
+  	  medium: new FormControl('Movies'),
+  	  name: new FormControl(''),
+  	  category: new FormControl(''),
+  	  year: new FormControl('')
+  	});
+  }
+
   onSubmit(mediaItem) {
     console.log(mediaItem);
   }
 }
+
+/*
+NOTE:
+Step 1: Define a FormGroup member variable and Initialize it with a model (An object of FormControls)
+Step 2-3: (In media-item-form.component.html)
+*/
