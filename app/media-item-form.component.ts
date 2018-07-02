56bd890c510d8956f7fa7a7df2935f1422d204ea
Fanchao92 <fanchaozhou1992@gmail.com>
2018-07-01 23:29:28 -0700

    Custom Validators

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mw-media-item-form',
@@ -12,10 +12,32 @@ export class MediaItemFormComponent {
   ngOnInit() {
   	this.form = new FormGroup({
   	  medium: new FormControl('Movies'),
-  	  name: new FormControl(''),
+  	  name: new FormControl('', Validators.compose([
+  	  	Validators.required,
+  	  	Validators.pattern('[\\w\\-\\s\\/]+'))
+  	  ]),
   	  category: new FormControl(''),
-  	  year: new FormControl('')
+  	  year: new FormControl('', this.yearValidator)
   	});
+  }
+
+  /*
+  NOTE:
+  Validators take as input a FormControl instance and returns either null if it’s valid or an error object if it’s invalid.
+  */
+  yearValidator(control) {
+  	if (control.value.trim().length === 0) {
+  		return null;
+  	} else {
+  		let year = parseInt(control.value);
+  		let minYear = 1900;
+  		let maxYear = 2100;
+  		if (year >= minYear && year <= maxYear) {
+  			return null;
+  		} else {
+  			return {'year': true};
+  		}
+  	}
   }
 
   onSubmit(mediaItem) {
@@ -25,6 +47,11 @@ export class MediaItemFormComponent {
 
 /*
 NOTE:
+Model-driven forms:
 Step 1: Define a FormGroup member variable and Initialize it with a model (An object of FormControls)
 Step 2-3: (In media-item-form.component.html)
+
+
+Validators:
+When a validator fails, the FormGroup variable this validator belongs to will set its "valid" property to false
 */
